799709f5bc2b1b58e25f1de8378af1fa1e4f7960
Paulo <paulo_rcampos@hotmail.com>
2025-10-05 15:28:40 -0300

    19 commit

--- dist/service/DeleteProductService.js
+++ dist/service/DeleteProductService.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,26 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.DeleteProductService = void 0;
 const prisma_1 = __importDefault(require("../prisma"));
 class DeleteProductService {
-    execute(_a) {
-        return __awaiter(this, arguments, void 0, function* ({ id }) {
-            if (!id) {
-                throw new Error("Solicitação inválida.");
+    async execute({ id }) {
+        if (!id) {
+            throw new Error("Solicitação inválida.");
+        }
+        const findProduct = await prisma_1.default.product.findFirst({
+            where: {
+                id: id
             }
-            const findProduct = yield prisma_1.default.product.findFirst({
-                where: {
-                    id: id
-                }
-            });
-            if (!findProduct) {
-                throw new Error("Cliente não existe.");
-            }
-            yield prisma_1.default.product.delete({
-                where: {
-                    id: findProduct.id
-                }
-            });
-            return { message: "Deletado com sucesso." };
         });
+        if (!findProduct) {
+            throw new Error("Cliente não existe.");
+        }
+        await prisma_1.default.product.delete({
+            where: {
+                id: findProduct.id
+            }
+        });
+        return { message: "Deletado com sucesso." };
     }
 }
 exports.DeleteProductService = DeleteProductService;
--- dist/service/ListPriceService.js
+++ dist/service/ListPriceService.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,11 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ListPriceService = void 0;
 const prisma_1 = __importDefault(require("../prisma"));
 class ListPriceService {
-    execute() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const price = yield prisma_1.default.price.findMany();
-            return price;
-        });
+    async execute() {
+        const price = await prisma_1.default.price.findMany();
+        return price;
     }
 }
 exports.ListPriceService = ListPriceService;
