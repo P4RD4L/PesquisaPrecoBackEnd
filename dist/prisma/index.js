af9592105b53de2c9af2b85cf784a28c044c5736
Paulo <paulo_rcampos@hotmail.com>
2025-10-05 14:04:43 -0300

    18 commit

--- dist/prisma/index.js
+++ dist/prisma/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_1 = require("../generated/prisma");
+const prisma_1 = require("../src/generated/prisma");
 const prismaClient = new prisma_1.PrismaClient();
 exports.default = prismaClient;
